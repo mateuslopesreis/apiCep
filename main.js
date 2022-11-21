a60ab440f73d7566246da187f5d98850e76fcf44
mateuslopesreis <mateus.reis@estudante.ifms.edu.br>
2022-11-21 15:07:10 -0300

    💄 atualização de interface

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
+import '@picocss/pico'
+
 const formConsultarCep = document.querySelector('#consultarCep')
-const inputCep = formConsultarCep.inputCep
+const inputCep = formConsultarCep.cep // seleciona o input do cep a partir do formulário
 const divDados = document.querySelector('#dados')
 
-formConsultarCep.addEventListener('submit', function(event){
-  event.preventDefault()//anula o comportamento padrao de envio do Form
-  consultarCep(inputCep.value)
+formConsultarCep.addEventListener('submit', function (event) {
+  event.preventDefault() // anula comportamento padrão de envio do form ao clicar no botão
+  consultarCep(inputCep.value) // invoca a função passando o cep digitado por parâmetro
 })
 
-
-async function consultarCep(cep){
- let response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
- let dadosCep = await response.json()
- divDados.innerHTML = `
- <p> Endereço: ${dadosCep.logradouro} </p>
- <p> Localidade: ${dadosCep.localidade} </p>
- 
- `
+async function consultarCep(cep) {
+  let response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+  let dadosCep = await response.json()
+  divDados.innerHTML = `
+    <p> Endereço: ${dadosCep.logradouro}  </p>
+    <p> Localidade: ${dadosCep.localidade}  </p>
+  `
 }
 
-consultarCep()
