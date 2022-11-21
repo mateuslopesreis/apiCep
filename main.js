9bfe10dd8dd1b8a72be20233b48572f8b5c4f800
mateuslopesreis <mateus.reis@estudante.ifms.edu.br>
2022-11-21 15:10:46 -0300

    ✨ adicionando loader no botao

--- main.js
+++ main.js
@@ -1,20 +1,45 @@
 import '@picocss/pico'
+import './style.css'
 
 const formConsultarCep = document.querySelector('#consultarCep')
 const inputCep = formConsultarCep.cep // seleciona o input do cep a partir do formulário
 const divDados = document.querySelector('#dados')
+const btnConsultarCep = document.querySelector('#btnConsultarCep')
+
+/* const loader = `<a href="#" aria-busy="true">Consultando Cep, aguarde...</a>` */
 
 formConsultarCep.addEventListener('submit', function (event) {
   event.preventDefault() // anula comportamento padrão de envio do form ao clicar no botão
+ 
+ ativaLoader(true)
   consultarCep(inputCep.value) // invoca a função passando o cep digitado por parâmetro
+
 })
 
 async function consultarCep(cep) {
   let response = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
   let dadosCep = await response.json()
+
+  if(dadosCep.erro){
+    divDados.innerHTML=`<div class="erro">Cep não encontrado!</div>`
+  }else{
+
   divDados.innerHTML = `
     <p> Endereço: ${dadosCep.logradouro}  </p>
     <p> Localidade: ${dadosCep.localidade}  </p>
   `
+  }
+  ativaLoader(false)
+}
+
+function ativaLoader(ativo){
+  if(ativo){
+    btnConsultarCep.setAttribute('arial-busy','true')
+    btnConsultarCep.textContent = 'Consultando CEP...'
+
+  }else{
+    btnConsultarCep.removeAttribute('aria-busy')
+    btnConsultarCep.textContent = 'Consultar'
+  }
 }
 
